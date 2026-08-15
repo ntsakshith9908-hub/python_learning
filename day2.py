e140c266dd15be4b05373ced2f7585052d8bd2b5
Your Name <you@example.com>
2026-08-15 22:06:55 +0530

    Tokens, Identifers, comments Learning practice

--- day2.py
+++ day2.py
@@ -1,64 +1,83 @@
-print("Non Sequences")
-#int
-q = 10
-print(type(q))
-#complex
-a = 3 + 4j
-print(a)
-print(type(a))
-b = 5j
-print(type(b))
-print(b)
-#bool
-c = True
-print(type(c))
+#Task1 Tokens and Statements
+name = "Rahul"
+age = 20
+city = "Hyderabad"
+print("Name: ", name)
+print("Age: ", age)
+print("City: ", city)
 
-d = None
-print(type(d))
+#Task2 Identifiers
+name1 = "Akshith"
+_age = 22
+sub_marks = 95
+print(name1)
+print(_age)
+print(sub_marks)
+'''
+Invalid Identifier because in variable creation we can not use special symbols expect underscore(_) 
+@marks = 80
+print(@marks)
+ '''
 
 
-print("Sequences")
-#List
-a = [1, 2, 3, 4]
-print(a)
-print(type(a))
-#Tuple
-b = (1, 2, 5, 6)
-print(b)
-print(type(b))
-#here paranthesis are optional
-b = 1, 2, 5, 6
-print(type(b))
+#Task3 Single-line comments
 
-# if one element in tuple use comma otherwise it considers as a integer
-a = 1
-print(type(a))
-a = 1,
-print(type(a))
-#set
-c = {1, 3, 5}
-print(type(c))
-#frozenset
-f = frozenset({1, 2, 4})
-print(type(f))
-#dict
-e = {'1': 'apple', '2': 'cherry', '3': 'mango'}
-print(e)
-print(type(e))
+name = "Varun"  # name is a variable and stores varun as value
+age = 24   # age is a variable and stores 24 as value
+print(name)
+print(age)
 
-r1 = range(1, 10, 2)
-print(*r1)
 
-r2 = range(10, 1, -3)
-print(*r2)
+#Task4 Multi-line comments
 
-a = list(range(10, 2, -2))
-print(a)
+# This is a programs that describe a simple python program.
+# It prints three messages on separate lines.
+# The program uses the print() function to display the messages
+print("Welcome to Python")
+print("I am learning programming")
+print("Python is easy to learn")
 
-print("Type Conversion")
-g = dict([['a', 1], ['b', 1], ['c', 1]])
-print(g)
+#Task5 Variables
 
-a = 1, 2, 3, 4
-a = list(a)
-print(a)
+name = "Akshith"
+age = 22
+height = 176
+is_student = True
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+#Task6 Multiple Assignment
+
+name, age, city = "Bhanu", 25, "Hyderabad"
+print("Name: ", name)
+print("Age: ", age)
+print("City: ", city)
+
+#Task7 Reassignment
+age = 22
+print(age)
+age = 24
+print(age)
+
+#Task8 Swapping  Variables
+a = 10
+b = 20
+print("Before: ", a, b)
+a, b = b, a
+print("After: ", a, b)
+
+#Task9 Deleting Variables
+name = "Akshith"
+print(name)
+del name
+print(name)
+
+
+#Task10 Keywords
+import keyword
+print("Python Keywords:")
+print(keyword.kwlist)
+
+print("Total: ", len(keyword.kwlist))
