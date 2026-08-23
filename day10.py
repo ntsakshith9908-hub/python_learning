3d16cdba48be6d951fd19d30de2a25f03d5fdb64
Your Name <you@example.com>
2026-08-23 19:19:54 +0530

    for loop with for each and range

--- day10.py
+++ day10.py
@@ -1,12 +1,55 @@
-# x = [2, 4, 5, 7]
-# for i in range(len(x)):
-#     print(i, x[i], end = " ")
+list = [4, 3, 2, 5, 6]
+n = len(list)
+#print elements in list with for each loop
+for num in list:
+    print(num)
+#print elements in list with index based for loop
+for i in range(n):
+    print(i, list[i])
 
+#skip printing even numbers in list
+for num in list:
+    if num % 2 == 0:
+        continue
+    print(num, end= " ")
+print()
 
-#Syntax for assert concept
-# assert condition
+#skip printing odd numbers in list
+for num in list:
+    if num % 2 == 1:
+        continue
+    print(num, end= " ")
+print()
 
+#when number 2 comes stop printing  
+for num in list:
+    if num == 2:
+        break
+    print(num, end= " ")
+print()
 
-n = 4
-assert n > 6 # n is not greater than 6
-print(True)
+#when first odd number comes stop printing
+for num in list:
+    if num % 2 == 1:
+        break
+    print(num, end= " ")
+print()
+
+#print numbers from 1 to 10, when all numbers are printed, print 'All numbers printed'
+for i in range(1, 11):
+    print(i)
+print("All numbers printed")
+
+#print numbers from 1 to 10, skipping even numbers, when all numbers are printed, print 'All
+#  numbers printed'
+for i in range(1, 11):
+    if i % 2 == 0:
+        continue
+    print(i)
+print("All numbers printed")
+#print numbers from 10 to 1, when 5 comes stop printing, when all numbers are print, print 'All numbers printed'
+for i in range(10, 0, -1):
+    if i == 5:
+        break
+    print(i)
+print("All numbers printed")
