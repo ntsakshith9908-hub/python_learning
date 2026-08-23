2d8de06074a58d92667645b04f45fd7d6ace66bc
Your Name <you@example.com>
2026-08-23 18:37:05 +0530

    conditional statements problems

--- day8.py
+++ day8.py
@@ -1,23 +1,67 @@
-# n = 5
-# if n % 2 != 0:
-#     print("Weird")
-# if n % 2 == 0 and 2 <= n <= 5:
-#     print("Not Weird")
-# elif n % 2 == 0 and 6 <= n <= 20:
-#     print("Weird")
-# else:
-#     print("Not Weird")
+#write the outputs before executing
+#TASK 1
+n = 9
+if n % 3 == 0:
+    print('A')
+print('Outside')
+'''
+Write Output: 
+A
+Outside
+'''
 
+#TASK 2:
+n = 10
+if n % 10 == 0:
+    print('A')
+if n % 5 == 0:
+    print('B')
+print('Outside')
+'''
+Write Output:
+A
+B
+Outside
+'''
 
+#TASK 3:
+n = 10 
+if n % 10 == 0:
+    print('A')
+elif n % 5 == 0:
+    print('B')
+print('Outside')
+'''
+Write Output:
+A
+Outside
+'''
 
-year = int(input("Enter the year: "))
-
-if year % 400 == 0:
-    print("Leap Year")
-elif year % 100 == 0:
-    print("Not a Leap Year")
-elif year % 4 == 0:
-    print("Leap Year")
+#TASK 4:
+n = 10 
+if n % 6 == 0:
+    print('A')
+elif n % 3 == 0:
+    print('B')
 else:
-    print("Not a Leap Year")
+    print('C')
+print('Outside')
+'''
+Write Output:
+C
+Outside
+'''
 
+#TASK 5:
+marks = 89 
+if marks > 40:
+    if marks > 75:
+        print('Dictinction') 
+    else:
+        print('Pass')
+else:
+    print('Fail')
+'''
+Write Output:
+Dictinction
+'''
